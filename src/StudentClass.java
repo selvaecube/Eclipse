bb8c14688b22809f32c437cd66a9bb7f92248ca8
selvaecube <51306245+selvaecube@users.noreply.github.com>
2019-06-18 20:54:23 +0530

    Add files via upload

---
+++ src/StudentClass.java
@@ -0,0 +1,18 @@
+package encapsulation;
+
+public class StudentClass {
+	private String name;//data member
+	//getter method for name
+	public String getName() {
+		return name;
+		
+	}
+      //setter method for name	
+          public void setName(String name) {
+        	  this.name=name;
+        	  
+          }
+	}
+
+	
+
