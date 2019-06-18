d571167430710c9003ec249e188255b2185618e8
selvaecube <51306245+selvaecube@users.noreply.github.com>
2019-06-18 20:53:39 +0530

    Add files via upload

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+package encapsulation;
+
+public class Test {
+
+	public static void main(String[] args) {
+		StudentClass r=new StudentClass();
+		r.setName("Selva");
+		System.out.println(r.getName());
+	}
+
+}
